a854e2f5f07bc1525da297cca4d3dd1dcd25fe8f
kaustav <kaustav.dey@pwc.com>
2020-08-23 15:52:05 +0530

    Initial Commit

---
+++ src/main/java/com/ope/ratelimiter/controller/RequestController.java
@@ -0,0 +1,21 @@
+package com.ope.ratelimiter.controller;
+import com.ope.ratelimiter.ratelimiter.RateLimiter;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/")
+
+public class RequestController {
+
+  @Autowired
+  RateLimiter rateLimiter;
+
+  @PostMapping(path="/isallowed")
+  public boolean isAllowed(@RequestHeader (name="userid")  String userId)
+  {
+    if (StringUtils.isEmpty(userId)) return false;
+    return rateLimiter.isAllowed(userId);
+  }
+}
---
+++ src/test/java/com/ope/ratelimiter/ratelimiter/RatelimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ope.ratelimiter.ratelimiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatelimiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
